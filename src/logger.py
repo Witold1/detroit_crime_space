0ceab1f87d6ca5c82f7246f0bbca0368fd81bfa7
Vit.Ye <yevtushenko.vit@gmail.com>
2020-06-22 19:34:20 -0400

    Changes in logger/prepare_logger function documentation

--- src/logger.py
+++ src/logger.py
@@ -3,16 +3,18 @@ LOG_FILENAME = 'db_logs.log'
 
 def prepare_logger(file_path : str = LOG_FILENAME) -> logging.RootLogger:
     '''
-    return logger object. Helps not to init logger each time in each module
+    return logger object. Helps not to init logger's config and naive import logging each time in each module
     # Logging HOWTO -> 
     # https://docs.python.org/3/howto/logging.html
     
+	* Now works with RootLogger, not Handlers.
+	
     :Parameters:
     
     input
         - :file_path: path to file with logs
     return
-        - :logger: ready to use in custom module logger object
+        - :logger: logger object which is ready to use in custom module
     '''
     logging.basicConfig(filename=file_path, level=logging.DEBUG, 
                     format="%(asctime)s %(levelname)s: %(message)s", datefmt='%m-%d-%Y %I:%M:%S %z')
